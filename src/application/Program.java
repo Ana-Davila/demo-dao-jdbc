782383f01264b4e2b2239e224e96f547954d9d88
Ana Davila Kulek <adkulek03@gmail.com>
2026-08-18 22:14:01 -0300

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,8 @@ public class Program {
 	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
-		/*System.out.println("=== TEST 1: seller findById ===");
+		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
@@ -40,7 +41,7 @@ public class Program {
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
-		System.out.println("Update Completed");*/
+		System.out.println("Update Completed");
 		
 		System.out.println("\n=== TEST 6 seller delete ===");
 		System.out.println("Enter id for delete teste: ");
